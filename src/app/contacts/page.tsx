9db9ab5888e9b76614871cca8526e4431365669f
Serhii Smilianets <smilyanets1991@gmail.com>
2025-04-29 12:14:11 +0200

    Added animation to Contact Page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -7,11 +7,13 @@ export default function ContactPage() {
     <div className={styles.contactPageContainer}>
       <h1 className='page-title'>Contact</h1>
       <div className={styles.contactPage}>
-        <div className={styles.container}>
+
+        <div className={`${styles.container} animate-fade-in-left`}>
           <ContactForm />
         </div>
         <div className={styles.divider} aria-hidden="true" />
-        <div className={styles.container}>
+
+        <div className={`${styles.container} animate-fade-in-right`}>
           <ContactInfo />
         </div>
       </div>
