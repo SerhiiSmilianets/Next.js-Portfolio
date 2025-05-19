6f1875d4fa3c1822ace7f8ab5dbb8b5c306669ce
Serhii Smilianets <smilyanets1991@gmail.com>
2025-05-19 14:50:34 +0200

    Updated Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
-import Image from 'next/image';
 import { FC } from 'react';
-import Avatar from '../../public/avatar.jpg';
-import styles from "@/styles/modules/home.module.css";
-// import { HeroSection } from '@/components/hero/HeroSection';
+import { HeroSection } from '@/components/hero/HeroSection';
 
 const Home: FC = () => {
   return (
     <>      
-      <div className={styles.profileImageContainer}>
-        <div className={`${styles.profileImageWrapper} animate-slideInTop`}>
-          <div className={styles.profileImageGlow}></div>
-          <Image
-            src={Avatar}
-            alt="Profile Picture"
-            fill
-            priority
-            className={styles.profileImage}
-          />
-        </div>
-      </div>
-      <div className={`${styles.profileInfoContainer} animate-slideInBot`}>
-        <h2 className={styles.profileName}>Serhii Smilianets</h2>
-        <p className={styles.certifiedText}>Certified Salesforce Commerce Cloud Developer</p>
-        <p className={styles.jobTitle}>Web developer</p>
-      </div>
-
-      {/* <HeroSection /> */}
+      <HeroSection />
     </>
   );
 };
