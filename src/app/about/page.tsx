a51691277bb7a5e14f3392c7bfee8535e6bd138f
Serhii Smilianets <smilyanets1991@gmail.com>
2025-05-22 18:13:18 +0200

    Updated contact section of CV template

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { WorkingExperience } from '@/components/about/experience/Experience'
-import {getExpYears} from '@/lib/dateHelper'
-import { getCompanies } from '@/lib/serverData';
+import { getCompanies, getSummaryInfo } from '@/lib/serverData';
 import {Company} from '@/interfaces';
 import Image from 'next/image';
 import Avatar from '../../../public/avatar.jpg';
@@ -14,8 +13,8 @@ export const metadata: Metadata = {
 };
 
 export default async function About() {
-  const experienceYears = getExpYears();
   const companies : Company[] = await getCompanies();
+  const summary : string = await getSummaryInfo();
 
   return (
     <div>
@@ -34,11 +33,7 @@ export default async function About() {
         </div>
         <div className="about-profile-description-container">
           <p className="about-profile-description">
-            I’m a Software Engineer with over {experienceYears} years of experience working with Salesforce Commerce Cloud (SFCC) as both a Front-End and Back-End Developer. I started my career in front-end development, focusing on JavaScript, jQuery, and SCSS, but eventually transitioned into back-end development, learning everything on my own. Over time, I became a Full-Stack Developer, handling everything from UI styling to complex server-side integrations.
-
-            I’ve worked on several high-profile eCommerce projects, including Continente, Bugaboo and Sephora. My responsibilities have ranged from bug fixing and performance optimization to implementing new features, integrating third-party services, and enhancing checkout processes. I’m skilled in JavaScript (both front-end and back-end), SFRA, SiteGenesis, Next.js, React.js, and Node.js.
-
-            I also hold a Salesforce Commerce Cloud Developer certification, and I’m always eager to expand my expertise. I thrive in dynamic environments, love solving complex problems, and enjoy collaborating with teams to build efficient and scalable solutions. My approach to work is detail-oriented, disciplined, and goal-driven, and I’m always open to learning something new.
+            {summary}
           </p>
         </div>
       </section>
